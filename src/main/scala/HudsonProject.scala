9aad3a5b35a634b52e0806cb97da964c32b06b57
Frederik Nordahl Sabroe <frederik.sabroe@bestbrains.dk>
2013-05-03 20:29:46 +0200

    Made the url a command-line parameter, and added an optional argument for the poll frequency in milliseconds

--- src/main/scala/HudsonProject.scala
+++ src/main/scala/HudsonProject.scala
@@ -1,3 +1,5 @@
+package dk.bestbrains.beacon
+
 case class HudsonBuild(number: Int, url: String)
 case class HudsonHealthReport(description: String, iconUrl: String, score: Int)
 class HudsonProject(val color: String, val lastBuild: HudsonBuild, val lastCompletedBuild: HudsonBuild) {
